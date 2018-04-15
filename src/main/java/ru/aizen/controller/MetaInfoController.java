d27135869be38e52a3fbf9e817c0d484f56bbdce
Aizen <symryvvin@gmail.com>
2018-04-15 21:26:43 +0300

    Fix string values

--- src/main/java/ru/aizen/controller/MetaInfoController.java
+++ src/main/java/ru/aizen/controller/MetaInfoController.java
@@ -1,5 +1,7 @@
 package ru.aizen.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.aizen.domain.character.Character;
@@ -7,6 +9,12 @@ import ru.aizen.domain.character.CharacterClass;
 
 @Component
 public class MetaInfoController extends AbstractController {
+    @FXML private Label title;
+    @FXML private Label name;
+    @FXML private Label level;
+    @FXML private Label characterClass;
+    @FXML private Label expansion;
+
     private final SkillsController skillsController;
     private final EditorController editorController;
 
@@ -17,6 +25,15 @@ public class MetaInfoController extends AbstractController {
         super(character);
         this.skillsController = skillsController;
         this.editorController = editorController;
+
+    }
+
+    public void initialize() {
+        title.textProperty().bind(character.titleProperty());
+        name.textProperty().bind(character.nameProperty());
+        level.textProperty().bind(character.levelProperty());
+        characterClass.textProperty().bind(character.classProperty());
+        expansion.textProperty().bind(character.expansionProperty());
     }
 
     @Override
