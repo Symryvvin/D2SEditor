18b9d0436651060e8f16b2cf6c35c843cd7175c8
Aizen <symryvvin@gmail.com>
2018-04-28 21:07:33 +0300

    Set double click action on list item in Backup Manager

--- src/main/java/ru/aizen/app/controller/BackupController.java
+++ src/main/java/ru/aizen/app/controller/BackupController.java
@@ -3,6 +3,7 @@ package ru.aizen.app.controller;
 import com.sun.javafx.collections.ObservableListWrapper;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import ru.aizen.domain.character.Character;
 import ru.aizen.domain.data.Backup;
 
@@ -34,10 +35,11 @@ public class BackupController {
         }
     }
 
-    @FXML
-    private void onRestoreClick() {
-        if (!list.getSelectionModel().isEmpty()) {
-            character.getCharacterData().setLastBackup(list.getSelectionModel().getSelectedItem());
+    public void onListClick(MouseEvent mouseEvent) {
+        if (mouseEvent.getClickCount() == 2) {
+            if (!list.getSelectionModel().isEmpty()) {
+                character.getCharacterData().setLastBackup(list.getSelectionModel().getSelectedItem());
+            }
         }
     }
 }
